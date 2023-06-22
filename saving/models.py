76899c2bb3a7f1b6decd1d9803ab11bddd7122a9
Travy Cent <travycent@travys-macbook-pro.kc.iuiu.ac.ug>
2023-06-22 18:38:04 +0300

    Set Up Django Project

---
+++ saving/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+#Transaction Type Model
+class transaction_types_model(models.Model):
+    transaction_type_id=models.AutoField(primary_key=True)
+    transaction_type_name=models.CharField(unique=True,max_length=10)
+    created_on= models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name_plural = 'Transaction Types'
+    def __str__(self):
+        return self.transaction_type_name
+# Percentage limits model
+class percentage_limits_model(models.Model):
+    percentage_id = models.AutoField(primary_key=True)
+    percentage = models.FloatField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name_plural = "Percentage Limits"
+    def __str__(self):
+        return self.percentage
+    
+    
