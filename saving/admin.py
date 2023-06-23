517a5dec0c0bfc69cae3762bb8a14ba737cc8d78
Travy Cent <travycent@travys-macbook-pro.kc.iuiu.ac.ug>
2023-06-23 16:45:52 +0300

    Added all Needed Models

--- saving/admin.py
+++ saving/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # Register your models here.
-from .models import transaction_types_model,percentage_limits_model
+from .models import transaction_types_model,percentage_limits_model,wallet_model,frequency_model,savings_preference_model,transactions_model,transactions_counter_model
 #Create Display Model Admin
 class transactionTypesAdmin(admin.ModelAdmin):
     #Specify data to be displayed in the list
@@ -9,7 +9,7 @@ class transactionTypesAdmin(admin.ModelAdmin):
     search_fields = ('transaction_type_name',)
     # Filter Data
     list_filter = ('created_on',)
-    
+# percentage limits   
 class percentageLimitsAdmin(admin.ModelAdmin):
     #Specify data to be displayed in the list
     list_display =('percentage','created_on')
@@ -17,10 +17,86 @@ class percentageLimitsAdmin(admin.ModelAdmin):
     search_fields = ('percentage',)
     # Filter Data
     list_filter = ('created_on',)
+# frequency limits   
+class frequencyAdmin(admin.ModelAdmin):
+    #Specify data to be displayed in the list
+    list_display =('frequency','created_on')
+    # Search Data
+    search_fields = ('frequency',)
+    # Filter Data
+    list_filter = ('created_on',)
+# User Wallets
+class walletsAdmin(admin.ModelAdmin):
+    # Exclude some fields from django Admin input
+    exclude = ('active_wallet_balance','saving_wallet_balance')
+    #Specify data to be displayed in the list
+    list_display= ('wallet_id','user_full_name','active_wallet_balance','saving_wallet_balance','wallet_update_date')
+    # Filter Data
+    list_filter = ('wallet_update_date',)
+    # Search Data
+    search_fields = ('user','active_wallet_balance','saving_wallet_balance')
+    # Function to return the fullname and add it to the list
+    def user_full_name(self, obj):
+        return obj.user.get_full_name()
+    # Sort the names based on firstname
+    user_full_name.admin_order_field = 'user__first_name'
+    # Display the fullname in the user section
+    user_full_name.short_description = 'User'
+
+# Savings Preference
+class savingsPreferenceAdmin(admin.ModelAdmin):
+    #Specify data to be displayed in the list
+    list_display= ('saving_preference_id','user_full_name','transaction_type_name','percentage','frequency','preference_update_date')
+    # Filter Data
+    list_filter = ('preference_update_date',)
+    # Search Data
+    search_fields = ('user','transaction_type_name','percentage','frequency')
+    # Function to return the fullname and add it to the list
+    def user_full_name(self, obj):
+        return obj.user.get_full_name()
+    # Sort the names based on firstname
+    user_full_name.admin_order_field = 'user__first_name'
+    # Display the fullname in the user section
+    user_full_name.short_description = 'User'
+# Transactions
+class transactionsAdmin(admin.ModelAdmin):
+    #Specify data to be displayed in the list
+    list_display= ('transaction_id','user_full_name','transaction_type_name','transaction_amount','transaction_desc','transaction_date')
+    # Filter Data
+    list_filter = ('transaction_date',)
+    # Search Data
+    search_fields = ('user','transaction_type_name','transaction_amount','transaction_desc')
+    # Function to return the fullname and add it to the list
+    def user_full_name(self, obj):
+        return obj.user.get_full_name()
+    # Sort the names based on firstname
+    user_full_name.admin_order_field = 'user__first_name'
+    # Display the fullname in the user section
+    user_full_name.short_description = 'User'
+
+# Transactions Counter
+class transactionsCounterAdmin(admin.ModelAdmin):
+    #Specify data to be displayed in the list
+    list_display= ('transaction_id','user_full_name','transaction_type_name','transaction_counter','counter_update_date')
+    # Filter Data
+    list_filter = ('counter_update_date',)
+    # Search Data
+    search_fields = ('user','transaction_type_name','transaction_counter')
+    # Function to return the fullname and add it to the list
+    def user_full_name(self, obj):
+        return obj.user.get_full_name()
+    # Sort the names based on firstname
+    user_full_name.admin_order_field = 'user__first_name'
+    # Display the fullname in the user section
+    user_full_name.short_description = 'User'
 
 #Register Models
 admin.site.register(transaction_types_model,transactionTypesAdmin)
 admin.site.register(percentage_limits_model,percentageLimitsAdmin)
-    
+admin.site.register(frequency_model,frequencyAdmin)
+admin.site.register(wallet_model,walletsAdmin)
+admin.site.register(savings_preference_model,savingsPreferenceAdmin)
+admin.site.register(transactions_model,transactionsAdmin)
+admin.site.register(transactions_counter_model,transactionsCounterAdmin)
     
 
--- saving/models.py
+++ saving/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+from django.core.exceptions import ValidationError
 
 #Transaction Type Model
 class transaction_types_model(models.Model):
@@ -16,8 +18,69 @@ class percentage_limits_model(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     class Meta:
         verbose_name_plural = "Percentage Limits"
+    def clean(self):
+        if self.percentage < 0 or self.percentage > 100:
+            raise ValidationError("Percentage should not be less than zero(0%) or greater than one hundred(100%).")
     def __str__(self):
         return str(self.percentage)
+    
+# Frequency model
+class frequency_model(models.Model):
+    frequency_id = models.AutoField(primary_key=True)
+    frequency = models.FloatField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name_plural = "Frequency"
+    def clean(self):
+        if self.frequency < 0 :
+            raise ValidationError("Frequency should not be less than zero(0).")
+    def __str__(self):
+        return str(self.frequency)
+# Savings Wallet Model
+class wallet_model(models.Model):
+    wallet_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    active_wallet_balance=models.FloatField(default=0.0)
+    saving_wallet_balance=models.FloatField(default=0.0)
+    wallet_update_date= models.DateTimeField(auto_now_add=True) 
+    class Meta:
+        verbose_name_plural = "User Wallets"
+    def __str__(self):
+        # return the fullname of users
+        return self.user.get_full_name() 
+# Saving Preferences Model
+class savings_preference_model(models.Model):
+    saving_preference_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    transaction_type_name=models.ForeignKey(transaction_types_model,on_delete=models.SET_NULL,blank=True,null=True)
+    percentage=models.ForeignKey(percentage_limits_model,on_delete=models.SET_NULL,blank=True,null=True)
+    frequency=models.ForeignKey(frequency_model,on_delete=models.SET_NULL,blank=True,null=True)
+    preference_update_date= models.DateTimeField(auto_now_add=True) 
+    class Meta:
+        verbose_name_plural = "Saving Preferences"
+        
+#Transactions Model
+class transactions_model(models.Model):
+    transaction_id=models.AutoField(primary_key=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,blank=True,null=True)
+    transaction_type_name=models.ForeignKey(transaction_types_model,on_delete=models.SET_NULL,blank=True,null=True)
+    transaction_amount=models.FloatField(default=0.0)
+    transaction_desc=models.CharField(default="Transaction Successful",max_length=100)
+    transaction_date= models.DateTimeField(auto_now_add=True) 
+    class Meta:
+        verbose_name_plural = 'Transactions'
+        
+#Transactions Counter Model
+class transactions_counter_model(models.Model):
+    transaction_id=models.AutoField(primary_key=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,blank=True,null=True)
+    transaction_type_name=models.ForeignKey(transaction_types_model,on_delete=models.SET_NULL,blank=True,null=True)
+    transaction_counter=models.IntegerField(default=0)
+    counter_update_date= models.DateTimeField(auto_now_add=True) 
+    class Meta:
+        verbose_name_plural = 'Transactions Counter'
+    
+
 
     
     
