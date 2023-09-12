0922661336b8ff2d1170f90571c8a914a1416831
Travy Cent <centtravy@gmail.com>
2023-09-12 20:32:34 +0300

    Added Auth Views and Serializers

---
+++ profiles/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers #import the serializer
+from .models import UsersModel
+class LoginSerializer(serializers.Serializer):
+    email = serializers.CharField()
+    password = serializers.CharField()
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+    phone_number = serializers.CharField(source='get_phone_number')
+
+    class Meta:
+        model = UsersModel
+        fields = ['id','email','phone_number','first_name','last_name','user_image','date_joined']
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+#Import all the models
+from .models import UsersModel
+from rest_framework import views, permissions, status
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.permissions import IsAdminUser,IsAuthenticated
+from rest_framework.response import Response
+from django.http import HttpResponseForbidden
 
-# Create your views here.
+from .serializers import LoginSerializer,UserSerializer
+from .authentication import JWTAuthentication
+
+User = get_user_model()
+
+class LoginView(views.APIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        email_or_phone_number = serializer.validated_data.get('username')
+        password = serializer.validated_data.get('password')
+
+        user = UsersModel.objects.filter(email=email_or_phone_number).first()
+        serializer_class=UserSerializer(user)
+        if user is None:
+            user = UsersModel.objects.filter(phone_number=email_or_phone_number).first()
+            
+
+        if user is None or not user.check_password(password):
+            return Response({'message': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Generate the JWT token
+        jwt_token = JWTAuthentication.create_jwt(user)
+
+        return Response({'token': jwt_token,'user_data' : serializer_class.data})
