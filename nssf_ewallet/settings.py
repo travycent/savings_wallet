26015f3773145b9677325f188e631f8289ffdf0f
Travy Cent <centtravy@gmail.com>
2023-09-12 21:46:49 +0300

    Changed trusted origins

--- nssf_ewallet/settings.py
+++ nssf_ewallet/settings.py
@@ -177,5 +177,6 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['http://13.246.166.173:9000']
 # Django Cors
 CORS_ORIGIN_ALLOW_ALL = True
